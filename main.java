0043b1bf9b7a194d6d29fe4909071593906c002e
Antonio Luis Garcia <antonlugar777@gmail.com>
2023-02-22 12:43:12 +0100

    generador de piramides

--- main.java
+++ main.java
@@ -1,6 +1,19 @@
-public class main{
-    public static void main(String[] args) {
-	System.out.print("Hola Mundo");
-	System.out.print("Vamos hacer merges en Git");
-	}
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("De que altura quieres la piramide");
+        int altura = sc.nextInt();
+   
+        for (int i = 1; i <= altura; i++) {
+            for (int j = i; j <altura ; j++){
+                System.out.print(" "); 
+            }
+            for (int k = 1; k < i*2 ; k++){
+                System.out.print("*"); 
+            }
+            System.out.println(" ");
+        }
+        sc.close();
+    }
 }
