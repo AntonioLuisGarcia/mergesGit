6b472a6e98e79ea1127e0d3cc9a7e5213148be68
daluga0503 <daniel.luquegallardo@gmail.com>
2023-02-22 18:38:13 +0100

    añadiendo coentario y quitando codigo

--- main.java
+++ main.java
@@ -1,11 +1,21 @@
+/* 
+* Ejercicio pirámide
+*
+* Dibuja una pirámide con asteriscos con una altura ingresada desde teclado.
+*
+* @author AntonioLuisGarcia
+* @author daluga0503
+*/
+
+
+
 import java.util.Scanner;
 public class main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("De que altura quieres la piramide");
         int altura = sc.nextInt();
-        System.out.print("Que caracter quieres utilizar para hacer la piramide");
-        String caracter = sc.next();
+
 	System.out.println("Aqui tienes la pirámide de " + altura + " de altura.");
         for (int i = 1; i <= altura; i++) {
             for (int j = i; j <altura ; j++){
